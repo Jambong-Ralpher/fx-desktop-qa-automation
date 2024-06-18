8ecd5ea7b570ddd6b772722dd105e747bb64e6c6
Sarina Li <sarinajin.li@gmail.com>
2024-06-18 10:45:43 -0400

    initial commit

--- tests/sync/test_create_firefox_account.py
+++ tests/sync/test_create_firefox_account.py
@@ -1,7 +1,9 @@
-from selenium.webdriver import Firefox
 from time import sleep
 
+from selenium.webdriver import Firefox
+
 from modules.page_object import AboutPrefs
+
 
 def test_create_firefox_account(driver: Firefox):
     about_prefs = AboutPrefs(driver, category="sync").open()
